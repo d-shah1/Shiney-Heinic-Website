9b07bf94dbc5d09bbd803533b6445f5e5a192db6
hetvishukla2001 <64137184+hetvishukla2001@users.noreply.github.com>
2021-10-06 10:42:03 +0530

    pdo

--- models/transaction.php
+++ models/transaction.php
@@ -11,35 +11,17 @@
     public function addTransactionFull($data) {
       // Prepare Query
       
-      $customer_id=$data['customer_id'];
-      $customer_name = $data['customer_name'];
-      $ship_add = $data['ship_add'];
-      $ship_country = $data['ship_country'];
-      $ship_state = $data['ship_state'];
-      $ship_zip = $data['ship_zip'];
-      $bill_add = $data['bill_add'];
-      $bill_country = $data['bill_country'];
-      $bill_state = $data['bill_state'];
-      $bill_zip = $data['bill_zip'];
-      $product_first = $data['product_first'];
-      $product_second = $data['product_second'];
-      $status = $data['status'];
-      $amount = $data['amount'];
-      $time = $data['time'];
-      $email = $data['email'];
-      $phone = $data['phone'];
-      $user_id = $data['user_id'];
+  
 
       $query = "INSERT INTO transaction ( `customer_id`, `customer_name`, `shiping_address`, `country`, `state`, `zip`, `bill_address`, `country_bill`, `state_bill`, `zip_bill`,`shiney_heinie_classic`, `shiney_no_the_go`, `status`, `amount`, `time_stamp`,`email`,`phone`,`user_id`)
-      VALUES('$customer_id', '$customer_name', '$ship_add', '$ship_country', '$ship_state', '$ship_zip', '$bill_add', 
-      '$bill_country', '$bill_state', '$bill_zip', $product_first,$product_second, '$status','$amount','$time','$email','$phone','$user_id')";
+      VALUES(:customer_id, :customer_name, :ship_add, :ship_country, :ship_state, :ship_zip, :bill_add, 
+      :bill_country, :bill_state, :bill_zip, :product_first,:product_second,  :status_new,:amount,:tme,:email,:phone,:u_id)";
 
-      
       $this->db->query($query);
 
       // Bind Values
      
-/*       $this->db->bind(':customer_id', $data['customer_id']);
+   $this->db->bind(':customer_id', $data['customer_id']);
       $this->db->bind(':customer_name', $data['customer_name']);
       $this->db->bind(':ship_add', $data['ship_add']);
       $this->db->bind(':ship_country', $data['ship_country']);
@@ -53,10 +35,10 @@
       $this->db->bind(':product_second', $data['product_second']);
       $this->db->bind(':status_new', $data['status']);
       $this->db->bind(':amount', $data['amount']);
-      $this->db->bind(':time',$data['time']);
+      $this->db->bind(':tme',$data['time']);
       $this->db->bind(':email', $data['email']);
       $this->db->bind(':phone', $data['phone']);
-      $this->db->bind(':u_id', $data['user_id']); */
+      $this->db->bind(':u_id', $data['user_id']); 
       // Execute
       if($this->db->execute()) {
         return true;
